d372cc726123f2974b06b4a3edfc7d2b7cf4de47
cbabi2023 <abijithcbbsccs2021@gmail.com>
2025-01-30 23:22:08 +0530

    Enhance Guru Arul feature: add new quotes, improve UI with gradient background, and update navigation to GuruarulPage

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,17 @@
 import 'package:flutter/material.dart';
 
-const Color backgroundColorLight = Color(0xFFEFEFEF); 
+const Color backgroundColorLight = Color(0xFFEFEFEF);
 const Color pureWhite = Color(0xFFFFFFFF);
+const Color containerGreen = Color(0xFF379777);
+const LinearGradient containerGradient = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [
+    Color(0xFF1A5D50), 
+    Color(0xFF379777), 
+    Color(0xFF123524), 
+    Color(0xFF3B7B66), 
+  ],
+  stops: [0.0, 0.4, 0.6, 1.0], 
+  tileMode: TileMode.mirror, 
+);
