897ce07bce5476f40899218556aeb0543d1281d8
Aswinazikar <azwinaswin8@gmail.com>
2025-01-30 12:36:13 +0530

    Refactor KrithiCategoryPage and SplashScreen class names for consistency; add initial tour configuration

--- lib/view/krithi_category.dart/krithi_category_page.dart
+++ lib/view/krithi_category.dart/krithi_category_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 import 'package:gurugranth/constants/constants.dart';
 import 'package:gurugranth/database/krithi_database.dart';
 import 'package:gurugranth/extensions/font_extensions/font_extensions.dart';
