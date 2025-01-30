8bcba6eafe755a12940759a219ad3eaa1edad529
Aswinazikar <azwinaswin8@gmail.com>
2025-01-30 10:21:59 +0530

    Enhance KrithiCategoryPage and KrithiPage with improved UI interactions and haptic feedback

--- lib/view/krithi_page/krithi_page.dart
+++ lib/view/krithi_page/krithi_page.dart
@@ -1,10 +1,12 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:gurugranth/database/krithi_database.dart';
+import 'package:gurugranth/extensions/font_extensions/font_extensions.dart';
 import 'package:gurugranth/view/krithi_page/krithi_page_controller.dart';
 import 'package:provider/provider.dart';
 
@@ -18,35 +20,50 @@ class KrithiPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-              providerObj.currentIndex = 0;
-            },
-            icon: Icon(Icons.arrow_back)),
+          icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () {
+            HapticFeedback.heavyImpact();
+            Navigator.pop(context);
+          },
+        ),
+        automaticallyImplyLeading: false,
+        shadowColor: Colors.grey,
+        backgroundColor: Colors.blue,
+        surfaceTintColor: Colors.white,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
         actions: [
           IconButton(
+              color: Colors.white,
               onPressed: () {
+                HapticFeedback.lightImpact();
                 providerObj.incrementIndex();
                 log(providerObj.currentIndex.toString());
               },
               icon: Icon(Icons.add_circle_outline)),
           SizedBox(
-            width: 10.h,
+            width: 10.w,
           ),
-          Text(providerObj.currentIndex.toString()),
+          Text(
+            providerObj.currentIndex.toString(),
+            style: context.anekMalayalamSemiBold12
+                .copyWith(color: Colors.white, fontSize: 12.sp),
+          ),
           SizedBox(
             width: 10.h,
           ),
           IconButton(
-              onPressed: () {
-                providerObj.decrementIndex();
-              },
-              icon: Icon(Icons.remove_circle_outline_sharp)),
+            color: Colors.white,
+            onPressed: () {
+              HapticFeedback.lightImpact();
+              providerObj.decrementIndex();
+            },
+            icon: Icon(Icons.remove_circle_outline_sharp),
+          ),
         ],
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: EdgeInsets.all(10.r),
           child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
