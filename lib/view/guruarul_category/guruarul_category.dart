e3df920c03e2875edf9d5f7edb5bbb69162cb06e
Aswinazikar <azwinaswin8@gmail.com>
2025-02-04 07:33:53 +0530

    Refactor GuruarulCategory: remove unused imports, simplify padding, and clean up item builder code

--- lib/view/guruarul_category/guruarul_category.dart
+++ lib/view/guruarul_category/guruarul_category.dart
@@ -3,10 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:gurugranth/constants/constants.dart';
 import 'package:gurugranth/database/guruarul_database.dart';
-import 'package:gurugranth/database/krithi_database.dart';
 import 'package:gurugranth/view/guruarul_page/guruarul_page.dart';
-import 'package:gurugranth/view/krithi_category.dart/krithi_category_page.dart';
-import 'package:gurugranth/view/krithi_page/krithi_page.dart';
 
 class GuruarulCategory extends StatelessWidget {
   const GuruarulCategory({super.key});
@@ -26,68 +23,48 @@ class GuruarulCategory extends StatelessWidget {
         ),
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(vertical: 10.0.h),
-        padding: EdgeInsets.symmetric(vertical: 10.0.h),
+        padding: EdgeInsets.symmetric(vertical: 10.0.r),
         child: ListView.separated(
-            itemBuilder: (context, index) => GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => GuruarulPage(indexCount: index),
-                          builder: (context) => GuruarulPage(indexCount: index),
-                        ));
-                  },
-                  child: Container(
-                    height: 50.h,
-                    padding: EdgeInsets.symmetric(horizontal: 10.0.w),
-                    decoration: BoxDecoration(
-                      color: pureWhite,
-                      border: Border(
-                        bottom: BorderSide(
-                            color: Colors.black.withValues(alpha: 0.1)),
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          "${index + 1}. ${GuruarulDatabase.guruArulData[index]['title']}",
-                          style: GoogleFonts.anekMalayalam(
-                              fontSize: 16.sp, fontWeight: FontWeight.w600),
-                          textAlign: TextAlign.left,
-                        ),
-                      ],
-                    ),
-                  child: Container(
-                    height: 50.h,
-                    padding: EdgeInsets.symmetric(horizontal: 10.0.w),
-                    decoration: BoxDecoration(
-                      color: pureWhite,
-                      border: Border(
-                        bottom: BorderSide(
-                            color: Colors.black.withValues(alpha: 0.1)),
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(
-                          "${index + 1}. ${GuruarulDatabase.guruArulData[index]['title']}",
-                          style: GoogleFonts.anekMalayalam(
-                              fontSize: 16.sp, fontWeight: FontWeight.w600),
-                          textAlign: TextAlign.left,
-                        ),
-                      ],
-                    ),
+          itemBuilder: (context, index) => GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => GuruarulPage(indexCount: index),
+                ),
+              );
+            },
+            child: Container(
+              height: 50.h,
+              padding: EdgeInsets.symmetric(horizontal: 10.0.w),
+              decoration: BoxDecoration(
+                color: pureWhite,
+                border: Border(
+                  bottom: BorderSide(
+                    color: Colors.black.withValues(alpha: .1),
                   ),
                 ),
-            separatorBuilder: (context, index) => SizedBox(
-                  height: 0.h,
-                  height: 0.h,
-                ),
-            itemCount: GuruarulDatabase.guruArulData.length),
-            itemCount: GuruarulDatabase.guruArulData.length),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(
+                    "${index + 1}. ${GuruarulDatabase.guruArulData[index]['title']}",
+                    style: GoogleFonts.anekMalayalam(
+                      fontSize: 16.sp,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    textAlign: TextAlign.left,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          separatorBuilder: (context, index) => SizedBox(
+            height: 0.h,
+          ),
+          itemCount: GuruarulDatabase.guruArulData.length,
+        ),
       ),
     );
   }
