81b2088411e1cdad50713aac07cdadb23c7657fa
cbabi2023 <abijithcbbsccs2021@gmail.com>
2025-02-05 17:09:05 +0530

    Integrate Appwrite SDK: configure client and account in main.dart, add callback activity in AndroidManifest.xml

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,29 @@ import 'package:gurugranth/routes/app_routes.dart';
 import 'package:gurugranth/view/krithi_page/krithi_page_controller.dart';
 import 'package:gurugranth/view/splash_screen/splash_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:appwrite/appwrite.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Client client = Client();
+  client
+      .setEndpoint('http://213.210.36.7/v1')
+      .setProject('67a348fe00034f374f49')
+      .setSelfSigned(
+          status:
+              true); // For self signed certificates, only use for development
+
+  Account account = Account(client);
+
+  runApp(MyApp(
+    account: account,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Account account;
+  const MyApp({super.key, required this.account});
 
   @override
   Widget build(BuildContext context) {
